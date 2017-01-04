edd7919ca2d0816060c119db8503cb4d6723fcbf
Stepan <xolms111@ya.ru>
2017-01-04 17:00:36 +0300

    gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,9 @@ concat         = require('gulp-concat'),
 uglify         = require('gulp-uglify'),
 cleanCSS       = require('gulp-clean-css'),
 rename         = require('gulp-rename'),
-del            = require('del'),
 jade           = require('gulp-jade'),
-imagemin       = require('gulp-imagemin'),
-pngquant       = require('imagemin-pngquant'),
 cache          = require('gulp-cache'),
 autoprefixer   = require('gulp-autoprefixer'),
-fileinclude    = require('gulp-file-include'),
-gulpRemoveHtml = require('gulp-remove-html'),
 bourbon        = require('node-bourbon'),
 ftp            = require('vinyl-ftp'),
 extender			 = require('gulp-html-extend'),
@@ -27,12 +22,7 @@ gulp.task('browser-sync', function() {
 		notify: false
 	});
 });
-gulp.task('extend', function () {
-    gulp.src('app/html/*.html')
-        .pipe(extender({annotations:true,verbose:false})) // default options 
-        .pipe(gulp.dest('app'))
- 
-});
+
 gulp.task('sass',  function() {
 	return gulp.src('app/sass/**/*.sass')
 	.pipe(sass({includePaths: [require('node-bourbon').includePaths, require('bourbon-neat').includePaths]}).on("error", notify.onError()))
@@ -44,16 +34,6 @@ gulp.task('sass',  function() {
 });
 
 
-gulp.task('libs', function() {
-	return gulp.src([
-		'app/libs/jquery/dist/jquery.min.js',
-		// 'app/libs/magnific-popup/magnific-popup.min.js'
-		])
-	.pipe(concat('libs.min.js'))
-	.pipe(uglify())
-	.pipe(gulp.dest('app/js'));
-});
-
 gulp.task('watch', ['sass', 'libs', 'browser-sync', 'jade'], function() {
 	gulp.watch('app/sass/**/*.sass', ['sass']);
 	gulp.watch('app/index.html', browserSync.reload);
@@ -62,25 +42,6 @@ gulp.task('watch', ['sass', 'libs', 'browser-sync', 'jade'], function() {
 	gulp.watch('app/jade/**/*.jade', ['jade']);
 });
 
-gulp.task('imagemin', function() {
-	return gulp.src('app/img/**/*')
-	.pipe(cache(imagemin({
-		interlaced: true,
-		progressive: true,
-		svgoPlugins: [{removeViewBox: false}],
-		use: [pngquant()]
-	})))
-	.pipe(gulp.dest('dist/img')); 
-});
-
-gulp.task('buildhtml', function() {
-	gulp.src(['app/html/*.html'])
-	.pipe(fileinclude({
-		prefix: '@@'
-	}))
-	.pipe(gulpRemoveHtml())
-	.pipe(gulp.dest('dist/index.html'))
-});
 gulp.task('jade', function() {
   return gulp.src('app/jade/*.jade')
     .pipe(jade({
@@ -89,23 +50,6 @@ gulp.task('jade', function() {
     //.pipe(obfuscate())
     .pipe(gulp.dest('app'))
     .pipe(browserSync.reload({stream: true}))
-});
-gulp.task('removedist', function() { return del.sync('dist'); });
-
-gulp.task('build', ['removedist', 'buildhtml', 'imagemin', 'sass', 'libs'], function() {
-
-	var buildCss = gulp.src([
-		'app/css/*.css'
-		]).pipe(gulp.dest('dist/css'));
-
-	var buildFiles = gulp.src([
-		'app/.htaccess'
-		]).pipe(gulp.dest('dist'));
-
-	var buildFonts = gulp.src('app/fonts/**/*').pipe(gulp.dest('dist/fonts'));
-
-	var buildJs = gulp.src('app/js/**/*').pipe(gulp.dest('dist/js'));
-
 });
 
 gulp.task('deploy', function() {
