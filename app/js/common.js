a42d1cd2f653e613f4664ee9e37dfa3eab66a679
Stepan <xolms111@ya.ru>
2017-01-31 16:18:08 +0300

    4646

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,151 @@
+function slider_port () {
+	var slidespeed = 700;
+	var timeout = 3000;
+	var links = true;
+	$('.portfolio__item').css(
+		{
+			"position":"absolute",
+			"top":'0',
+			"left":'0'
+		}).hide().eq(0).show();
+	var slidenum = 0;
+	var slidetime;
+	var slideCount = $('.portfolio__slider .portfolio__item').length;
+	var animslide = function (arrow) {
+		clearTimeout(slidetime);
+		$('.portfolio__item').eq(slidenum).fadeOut(slidespeed);
+		if(arrow=="next")
+		{
+			if(slidenum == (slideCount-1))
+			{
+				slidenum=0;
+			}
+			else
+			{
+				slidenum++;
+			}
+		}
+		else if (arrow == "prew")
+		{
+			if(slidenum == 0)
+			{
+				slidenum=slideCount-1;
+			}
+			else
+			{
+				slidenum-=1;
+			}
+		}
+		else
+		{
+			slidenum = arrow;
+		}
+		$('.portfolio__item').eq(slidenum).fadeIn(slidespeed, rotator);
+	}
+	if(links)
+	{
+		$('.portfolio__buttons--next').click(function () {
+		   animslide("next");
+		});
+		$('.portfolio__buttons--prev').click(function () {
+		   animslide("prew");
+		});
+	}
 
+	var pause = false;
+	var rotator = function () {
+		if(!pause)
+		{
+			slidetime = setTimeout(function () {
+			   animslide('next')}, timeout);
+		}
+	}
+	$('.portfolio__slider').hover(
+		function () {
+			clearTimeout(slidetime);
+			pause = true;
+		},
+		function () {
+			pause = false;
+			rotator();
+		}
+	);
+	rotator();
+}
+function slider_rev () {
+	var slidespeed = 700;
+	var timeout = 3000;
+	var links = true;
+	$('.reviews__item').css(
+		{
+			"position":"absolute",
+			"top":'0',
+			"left":'0'
+		}).hide().eq(0).show();
+	var slidenum = 0;
+	var slidetime;
+	var slideCount = $('.reviews__slider .reviews__item').length;
+	var animslide = function (arrow) {
+		clearTimeout(slidetime);
+		$('.reviews__item').eq(slidenum).fadeOut(slidespeed);
+		if(arrow=="next")
+		{
+			if(slidenum == (slideCount-1))
+			{
+				slidenum=0;
+			}
+			else
+			{
+				slidenum++;
+			}
+		}
+		else if (arrow == "prew")
+		{
+			if(slidenum == 0)
+			{
+				slidenum=slideCount-1;
+			}
+			else
+			{
+				slidenum-=1;
+			}
+		}
+		else
+		{
+			slidenum = arrow;
+		}
+		$('.reviews__item').eq(slidenum).fadeIn(slidespeed, rotator);
+	}
+	if(links)
+	{
+		$('.reviews__buttons--next').click(function () {
+		   animslide("next");
+		});
+		$('.reviews__buttons--prev').click(function () {
+		   animslide("prew");
+		});
+	}
+
+	var pause = false;
+	var rotator = function () {
+		if(!pause)
+		{
+			slidetime = setTimeout(function () {
+			   animslide('next')}, timeout);
+		}
+	}
+	$('.reviews__slider').hover(
+		function () {
+			clearTimeout(slidetime);
+			pause = true;
+		},
+		function () {
+			pause = false;
+			rotator();
+		}
+	);
+	rotator();
+}
 
 function slider_image () {
 	var slidespeed = 700;
@@ -48,7 +195,7 @@ function slider_image () {
 	}
 	if(links)
 	{
-		var $linkarrow = $('<a id="prewbutton" href="#" class="slider-images__button slider-images__button--prev"></a><a href="#" id="nextbutton" class="slider-images__button slider-images__button--next"></a>').prependTo('.slider-images__container');
+		var $linkarrow = $('<div id="prewbutton" class="slider-images__button slider-images__button--prev"></div><div id="nextbutton" class="slider-images__button slider-images__button--next"></div>').prependTo('.slider-images__container');
 		$('#nextbutton').click(function () {
 		   animslide("next");
 		});
@@ -76,7 +223,7 @@ function slider_image () {
 			   animslide('next')}, timeout);
 		}
 	}
-	$('.slider-images__container').hover(
+	$('.slider-images').hover(
 		function () {
 			clearTimeout(slidetime);
 			pause = true;
@@ -108,29 +255,9 @@ function links_resize () {
 
 $(document).ready(function(){
 	slider_image();
+	slider_rev();
+	slider_port();
 	$(window).resize(function(){
 		links_resize();
 	});
-	var owl = $('.reviews__slider');
-	owl.owlCarousel({
-		loop:true,
-		responsiveClass:true,
-		items:1,
-		autoplay:true,
-	});
-	owl.on("mousewheel", ".owl-wrapper", function (e) {
-    if (e.deltaY > 0) {
-      owl.trigger("owl.prev");
-    } else {
-      owl.trigger("owl.next");
-    }
-    e.preventDefault();
-  });
-	$(".reviews__buttons--next").click(function() {
-    owl.trigger('next.owl.carousel');
-    console.log(123);
-  });
-  $(".reviews__buttons--prev").click(function() {
-    owl.trigger('prev.owl.carousel');
-  });
 });
